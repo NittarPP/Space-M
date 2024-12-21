6d04c0edcb9413a6ff25084cbc897cd0507f0a82
NittarPP <167613469+NittarPP@users.noreply.github.com>
2024-12-21 11:46:32 +0700

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,11 @@
+--[[ 
+◀ DO NOT'T USE IT ▶
+If you use this and don't credit me, i can DMCA you.
+
+◀ This Gui Make For Space M ▶
+]]
+-- ◀ Script ▶
+
+print("oh no i don't have any script")
+
+-- ◀ End Script ▶
